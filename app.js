624ad127c68151795cca1303fb99ac420501401d
benyamynbrkyc <benjamin.brkic@gmail.com>
2020-03-15 18:10:34 +0100

    init commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+//var server = express.createServer();
+// express.createServer()  is deprecated.
+const app = express(); // better instead
+
+app.use(express.static(__dirname + '/public'));
+
+const PORT = process.env | 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname, '/public/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log('Server listening on port,', PORT);
+});
