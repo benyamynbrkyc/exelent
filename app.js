0bad30da9f1f7a6abb27ff65e84dc5d6c8a78229
benyamynbrkyc <benjamin.brkic@gmail.com>
2020-06-08 14:12:16 +0200

    early draft, isa-erp page done

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require('express')
+const path = require('path')
+const app = express()
 
-const translateRoutes = require('./translator');
+const translateRoutes = require('./translator')
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')))
 
-app.use('/translate', translateRoutes);
+app.use('/translate', translateRoutes)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname, '/public/index.html');
-});
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.get('/isaerp', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/isa-erp.html'))
+})
 
 app.listen(PORT, () => {
-  console.log('Server listening on port,', PORT);
-});
+  console.log('Server listening on port,', PORT)
+})
