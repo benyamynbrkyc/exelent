1a4b09b78377120b8177575b01abd08bcb7b950f
brkic.benjamin <brkic.benjamin@nsoft.com>
2024-08-28 23:09:32 +0200

    add mobile menu content

--- src/app/nav.tsx
+++ src/app/nav.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { Images, Menu } from "lucide-react";
+import { Menu } from "lucide-react";
 import Link from "next/link";
 
 export function Nav() {
@@ -9,21 +9,8 @@ export function Nav() {
       <div className="font-bold">Coming Soon</div>
       {/* desktop menu */}
       <div className="hidden items-center gap-44 lg:flex">
-        <nav className="flex items-center gap-4">
-          <Button asChild variant={"outline"}>
-            <Link href={"#"}>Home</Link>
-          </Button>
-          <Button asChild variant={"outline"}>
-            <Link href={"#"}>ISA-ERP</Link>
-          </Button>
-          <Button asChild variant={"outline"}>
-            <Link href={"#"}>Contact</Link>
-          </Button>
-        </nav>
-        {/* todo: lang switcher */}
-        <div className="text-foreground">
-          ENG | <span className="text-muted-foreground">BOS</span>
-        </div>
+        <Links />
+        <LangSwitcher />
       </div>
       {/* mobile menu */}
       <div className="block lg:hidden">
@@ -31,9 +18,41 @@ export function Nav() {
           <SheetTrigger>
             <Menu className="h-8 w-8" />
           </SheetTrigger>
-          <SheetContent>hello</SheetContent>
+          <SheetContent>
+            <div className="mt-8 space-y-10">
+              <Links />
+              <LangSwitcher />
+            </div>
+          </SheetContent>
         </Sheet>
       </div>
+    </div>
+  );
+}
+
+function Links() {
+  return (
+    <nav className="flex flex-col gap-4 lg:flex-row lg:items-center">
+      <Button asChild variant={"outline"}>
+        <Link href={"#"}>Home</Link>
+      </Button>
+      <Button asChild variant={"outline"}>
+        <Link href={"#"}>ISA-ERP</Link>
+      </Button>
+      <Button asChild variant={"outline"}>
+        <Link href={"#"}>Contact</Link>
+      </Button>
+    </nav>
+  );
+}
+
+{
+  /* todo: lang switcher */
+}
+function LangSwitcher() {
+  return (
+    <div className="text-foreground">
+      ENG | <span className="text-muted-foreground">BOS</span>
     </div>
   );
 }
