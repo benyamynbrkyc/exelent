f47a56fed94aed6927318c065124e051da81e81d
brkic.benjamin <brkic.benjamin@nsoft.com>
2024-08-28 23:05:51 +0200

    add coming soon

--- src/app/nav.tsx
+++ src/app/nav.tsx
@@ -1,25 +1,38 @@
 import { Button } from "@/components/ui/button";
-import { Images } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Images, Menu } from "lucide-react";
 import Link from "next/link";
 
 export function Nav() {
   return (
-    <div className="sticky top-0 isolate z-50 flex items-center justify-center gap-44 p-10">
-      <Images className="h-8 w-8" size={24} />
-      <div className="flex items-center gap-44">
+    <div className="sticky top-0 isolate z-50 flex items-center justify-between p-10 lg:justify-center lg:gap-44">
+      <div className="font-bold">Coming Soon</div>
+      {/* desktop menu */}
+      <div className="hidden items-center gap-44 lg:flex">
         <nav className="flex items-center gap-4">
           <Button asChild variant={"outline"}>
-            <Link href={"/"}>Home</Link>
+            <Link href={"#"}>Home</Link>
           </Button>
           <Button asChild variant={"outline"}>
-            <Link href={"/contact"}>Contact</Link>
+            <Link href={"#"}>ISA-ERP</Link>
           </Button>
           <Button asChild variant={"outline"}>
-            <Link href={"/about"}>About</Link>
+            <Link href={"#"}>Contact</Link>
           </Button>
         </nav>
         {/* todo: lang switcher */}
-        <div>ENG | BOS</div>
+        <div className="text-foreground">
+          ENG | <span className="text-muted-foreground">BOS</span>
+        </div>
+      </div>
+      {/* mobile menu */}
+      <div className="block lg:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <Menu className="h-8 w-8" />
+          </SheetTrigger>
+          <SheetContent>hello</SheetContent>
+        </Sheet>
       </div>
     </div>
   );
